f14c5dcb85fe969026d4c438278eb30683fc1b68
jesuarman02 <jesuarman74@gmail.com>
2026-04-16 14:59:27 -0600

    Cambios en Sidebar

--- gateway/src/main/webapp/app/intefaz/sidebarModule.vue
+++ gateway/src/main/webapp/app/intefaz/sidebarModule.vue
@@ -3,141 +3,80 @@
     <nav class="sidebar-icons">
       <div class="icon-group">
 
-                <div class="icon-item icon-item-custom icon-yellow" @click="$emit('toggle-search')">
+        <div class="icon-item icon-yellow" @click="$emit('toggle-search')" title="Buscar Paciente">
           <img src="/content/images/md-del-usuario.svg" class="sidebar-svg" />
         </div>
-          
-          <div class="icon-item icon-item-custom icon-blue" @click="irCalendario">
-            <img src="/content/images/calendario.svg" class="sidebar-svg" />
-          </div>
-          
-          <div class="icon-item icon-item-custom icon-green" @click="$emit('update:mostrarSubirArchivos', true)">
-            <img src="/content/images/carpeta.svg" class="sidebar-svg" />
-          </div>
-          
-          <div class="icon-item icon-item-custom icon-blue" @click="$emit('toggle-clipboard')">
-            <img src="/content/images/clipboard.svg" class="sidebar-svg"/>
-          </div>
-          
-          <!-- Los que ya tenías -->
-          <div class="icon-item icon-item-custom icon-yellow" @click="abrirModalPrueba">
-            <span style="font-size: 1.5rem;">🧑‍⚕️</span>
-          </div>
-          
-          <div class="icon-item icon-item-custom icon-blue" @click="abrirModalDireccion">
-            <span style="font-size: 1.5rem;">📍</span>
-          </div>
-          
-          <div class="icon-item icon-item-custom icon-green" @click="abrirModalWizard">
-    <span style="font-size: 1.5rem;">📋</span>
-  </div>
+        
+        <div class="icon-item icon-blue" @click="irCalendario" title="Calendario">
+          <img src="/content/images/calendario.svg" class="sidebar-svg" />
         </div>
+        
+        <div class="icon-item icon-green" @click="$emit('update:mostrarSubirArchivos', true)" title="Expedientes">
+          <img src="/content/images/carpeta.svg" class="sidebar-svg" />
+        </div>
+        
+        <div class="icon-item icon-blue" @click="$emit('toggle-clipboard')" title="Asignaciones">
+          <img src="/content/images/clipboard.svg" class="sidebar-svg"/>
+        </div>
+        
+        <div class="icon-separator"></div>
+
+        <div class="icon-item icon-yellow" @click="abrirModalPrueba" title="Modal Paciente">
+          <span class="emoji-icon">🧑‍⚕️</span>
+        </div>
+        
+        <div class="icon-item icon-blue" @click="abrirModalDireccion" title="Modal Dirección">
+          <span class="emoji-icon">📍</span>
+        </div>
+        
+        <div class="icon-item icon-green" @click="abrirModalWizard" title="Estudio Socioeconómico">
+          <span class="emoji-icon">📋</span>
+        </div>
+
+      </div>
     </nav>
 
-    <PacienteModal 
-      v-model:visible="mostrarModalPaciente" 
-      @save="simularGuardado" 
-    />
-
-    <DireccionModal 
-      v-model:visible="mostrarModalDireccion" 
-      @saved="simularGuardadoDireccion" 
-    />
-
-    <InfoSocioeconomicaWizardModal 
-      v-model:visible="mostrarModalWizard" 
-      @saved="simularGuardadoWizard" 
-    />
+    <PacienteModal v-model:visible="mostrarModalPaciente" @save="simularGuardado" />
+    <DireccionModal v-model:visible="mostrarModalDireccion" @saved="simularGuardadoDireccion" />
+    <InfoSocioeconomicaWizardModal v-model:visible="mostrarModalWizard" @saved="simularGuardadoWizard" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import { useRouter, useRoute } from 'vue-router'
-
-// Importamos los componentes
 import PacienteModal from '@/shared/modals/PacienteModal.vue'
 import DireccionModal from '@/shared/modals/DireccionModal.vue'
 import InfoSocioeconomicaWizardModal from '@/shared/modals/info-socioeconomica-wizard.vue'
 
 export default defineComponent({
   name: 'SidebarModule',
-  
-  components: {
-    PacienteModal,
-    DireccionModal,
-    InfoSocioeconomicaWizardModal
-  },
-
-  props: {
-    mostrarHeader: Boolean,
-    mostrarSubirArchivos: Boolean
-  },
-  
-  // 🔥 Fusionamos todos los "emits" (Los de la nube y los tuyos)
-  emits: [
-    'toggle-search', 
-    'update:mostrarSubirArchivos', 
-    'toggle-nuevo', 
-    'toggle-clipboard'
-  ],
+  components: { PacienteModal, DireccionModal, InfoSocioeconomicaWizardModal },
+  props: { mostrarHeader: Boolean, mostrarSubirArchivos: Boolean },
+  emits: ['toggle-search', 'update:mostrarSubirArchivos', 'toggle-nuevo', 'toggle-clipboard'],
   
   setup(props, { emit }) {
-    const router = useRouter()
-    const route = useRoute()
-    
-    // Variables reactivas para controlar si los modales se ven o no
-    const mostrarModalPaciente = ref(false)
-    const mostrarModalDireccion = ref(false)
-    const mostrarModalWizard = ref(false) 
+    const router = useRouter(); const route = useRoute();
+    const mostrarModalPaciente = ref(false);
+    const mostrarModalDireccion = ref(false);
+    const mostrarModalWizard = ref(false);
 
-    const irCalendario = () => {
-      if (route.path === '/calendario'){
-        router.back()
-      } else {
-        router.push('/calendario')
-      }
-    }
+    const irCalendario = () => { route.path === '/calendario' ? router.back() : router.push('/calendario') }
+    const abrirModalPrueba = () => mostrarModalPaciente.value = true;
+    const abrirModalDireccion = () => mostrarModalDireccion.value = true;
+    const abrirModalWizard = () => mostrarModalWizard.value = true;
 
-    const abrirModalPrueba = () => {
-      mostrarModalPaciente.value = true
-    }
-
-    const abrirModalDireccion = () => {
-      mostrarModalDireccion.value = true
-    }
-
-    const abrirModalWizard = () => {
-      mostrarModalWizard.value = true
-    }
-
-    const simularGuardado = (datosDelPaciente: any) => {
-      console.log("¡Éxito! El modal mandó estos datos listos para enviar a Java:", datosDelPaciente)
-      alert("Revisa la consola (F12) para ver los datos capturados.")
-    }
-
-    const simularGuardadoDireccion = () => {
-      console.log("¡El modal de dirección finalizó su guardado con éxito!")
-    }
-
-    const simularGuardadoWizard = () => {
-      console.log("¡El estudio socioeconómico finalizó y se guardó con éxito!")
-    }
+    const simularGuardado = (datos: any) => console.log("Guardado:", datos);
+    const simularGuardadoDireccion = () => console.log("Dirección guardada");
+    const simularGuardadoWizard = () => console.log("Socioeconómico guardado");
 
     return {
-      irCalendario,
-      mostrarModalPaciente,
-      abrirModalPrueba,
-      simularGuardado,
-      mostrarModalDireccion,
-      abrirModalDireccion,
-      simularGuardadoDireccion,
-      mostrarModalWizard,
-      abrirModalWizard,
-      simularGuardadoWizard
+      irCalendario, mostrarModalPaciente, abrirModalPrueba, simularGuardado,
+      mostrarModalDireccion, abrirModalDireccion, simularGuardadoDireccion,
+      mostrarModalWizard, abrirModalWizard, simularGuardadoWizard
     }
   }
-}) // Eliminé la coma extra que había quedado al final aquí
+})
 </script>
 
 <style scoped src="../../content/css/navbar.css"></style>
